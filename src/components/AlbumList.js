785560e153c799332982ab0d2eb7afcd00a5e72b
Aaron Gottlieb <atticus19@gmail.com>
2017-07-19 16:23:57 -0600

    adds header and albumlist componentes

---
+++ src/components/AlbumList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+
+  componentWillMount() {
+    console.log('component will mount')
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List!!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
