817d0e11f31dc6e2551bc9ff6dd1b537a313d48a
Aaron Gottlieb <atticus19@gmail.com>
2017-07-25 16:43:24 -0600

    adds Linking to button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
@@ -12,7 +12,7 @@ const AlbumDetail = ({ album }) => {
     headerTextStyle,
     imageStyle
    } = styles;
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   return (
     <Card>
       <CardSection>
@@ -31,7 +31,9 @@ const AlbumDetail = ({ album }) => {
         <Image style={imageStyle} source={{ uri: image }} />
       </CardSection>
       <CardSection>
-        <Button />
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy {title} 
+        </Button>
       </CardSection>
     </Card>
 
